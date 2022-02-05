ecf7b2e215f10b2944936bdf80960736af98c7d8
ben-greenwood <ben.224@hotmail.co.uk>
2022-02-05 18:35:23 +0000

    Bio update & prettier linting

--- components/Article.js
+++ components/Article.js
@@ -1,29 +1,42 @@
 import Image from "next/image"
-
+const classNames = require("classnames")
 
 const Article = ({ img, title, url, className }) => {
-  new URL(url);
+  new URL(url)
   const urlHost = new URL(url).hostname
-  
+
   return (
-    <a href={url} className={className + " " + "group bg-gray-100 rounded-lg overflow-hidden flex flex-col"}>
-      <div className="w-full aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
-        <img src={img} alt={title} className="w-full h-32 object-center object-cover group-hover:opacity-75" />
+    <a
+      href={url}
+      className={classNames(
+        { className },
+        "group grid grid-cols-5 overflow-hidden rounded-md border border-gray-100 bg-white p-2 transition-transform duration-200 ease-in-out hover:scale-105"
+      )}
+    >
+      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full">
+        <img
+          src={img}
+          alt={title}
+          className="h-14 w-full overflow-hidden rounded-md object-cover object-center"
+        />
       </div>
-      <div className="p-4 flex flex-col space-between h-full flex-grow justify-between">
-        <h3 className="text-sm font-medium text-gray-600">
-          {title}
-        </h3>
-        <span className="text-xs text-gray-400 flex items-center mt-2">
+      <div className="col-span-4 ml-2 p-2">
+        <h3 className="truncate text-sm font-medium text-gray-700">{title}</h3>
+        <span className="mt-1 flex items-center text-xs text-gray-400">
           {urlHost}
-          <svg xmlns="http://www.w3.org/2000/svg" className="ml-1 h-3 w-3 fill-current text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ml-1 h-3 w-3 fill-current text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
             <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
             <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
           </svg>
         </span>
       </div>
     </a>
-  );
+  )
 }
 
-export default Article;
+export default Article
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
-import Footer from '../components/Footer'
-import Header from '../components/Home/Header'
-import Bio from '../components/Home/Bio'
-import ReccomendedReading from '../components/Home/ReccomendedReading'
+import Footer from "../components/Footer"
+import Header from "../components/Home/Header"
+import Bio from "../components/Home/Bio"
+import ReccomendedReading from "../components/Home/ReccomendedReading"
 
 export default function Home() {
   return (
@@ -16,21 +16,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <body className="min-h-screen dark:bg-slate-900">
-        <main className="flex flex-col px-6 mx-auto max-w-7xl">
-          <Header/>
+      <body className="min-h-screen bg-white dark:bg-slate-900">
+        <main className="mx-auto flex max-w-7xl flex-col px-6">
+          <Header />
           <div className="flex-grow gap-8 lg:grid lg:grid-cols-12">
             <div className="col-span-7 my-4 md:my-8">
-              <Bio/>
+              <Bio />
             </div>
-            <div className="col-span-5 pt-8 mt-8 border-t lg:mt-0 lg:pl-8 border-slate-100 lg:border-0">
+            <div className="col-span-5 mt-8 border-t border-slate-100 pt-8 lg:mt-0 lg:border-0 lg:pl-8">
               <div className="space-y-8">
                 <ReccomendedReading />
               </div>
             </div>
           </div>
         </main>
-        <Footer/>
+        <Footer />
       </body>
     </div>
   )
