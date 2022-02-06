5ff54ad1ee85152346b74514431330715850a833
ben-greenwood <ben.224@hotmail.co.uk>
2022-02-06 16:25:09 +0000

    Update bio copy

--- components/Home/Bio.js
+++ components/Home/Bio.js
@@ -3,21 +3,21 @@ import ExternalLink from "../ExternalLink"
 const Bio = () => (
   <div className="prose prose-orange max-w-none space-y-10 dark:prose-invert dark:prose-orange">
     <p>
-      I&apos;m a Product Engineer based in Bath UK, currently working at{" "}
+      <span className="mr-1 text-lg">👋</span> I&apos;m a Product Engineer based
+      in Bath UK, currently working at{" "}
       <ExternalLink url="https://appearhere.co.uk" text="Appear [ here ]" />,
       where I&apos;m helping make flexible retail more accessible.
     </p>
     <p>
-      Previously I worked at <ExternalLink url="https://mmtm.io" text="mmtm" />,
-      where I helped build web/mobile apps for companies spanning multiple
-      industries from Telecoms to VR. I also had the opportunity to lead the
-      design and run Product Design Sprints for several projects during my time
-      there.
+      Previously, I worked at <ExternalLink url="https://mmtm.io" text="mmtm" />{" "}
+      - a small digital agency based out of Cheltenham - where I spent most of
+      my time designing, prototyping and building web/mobile apps for companies
+      spanning multiple industries from Telecoms to VR.
     </p>
     <p>
       I enjoy writing code, but my passion is in building useful products. I
       love sweating the details of a design and making each interaction as
-      smooth and effortless as can be.
+      smooth and effortless as possible.
     </p>
     <div className="hidden grid-cols-2 sm:grid">
       <div>
