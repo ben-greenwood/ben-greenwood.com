6f3a4533007437821373a41624da7914a31a3008
ben-greenwood <ben.224@hotmail.co.uk>
2022-02-20 12:50:25 +0000

    Add Framer animations and replace inline SVGs

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -1,11 +1,11 @@
 const Header = () => (
-  <div className="py-6 border-b border-slate-100 dark:border-slate-700 lg:pt-10 lg:pb-6">
-    <div className="flex items-center justify-between mt-4">
+  <div className="border-b border-slate-100 py-6 dark:border-slate-700 lg:pt-10 lg:pb-6">
+    <div className="mt-4 flex items-center justify-between">
       <div className="">
-        <h1 className="text-2xl font-black md:text-4xl font-heading text-slate-800 dark:text-slate-200">
+        <h1 className="font-heading text-2xl font-black text-slate-800 dark:text-slate-200 md:text-4xl">
           Ben Greenwood
         </h1>
-        <h2 className="text-base font-semibold md:text-lg text-slate-700 dark:text-slate-300">
+        <h2 className="text-base font-semibold text-slate-700 dark:text-slate-300 md:text-lg">
           Product Engineer
         </h2>
       </div>
