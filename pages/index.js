b5014882d13a81cb5834ad1b15e4f1bbe2cffe0a
ben-greenwood <bengreenwood@Bens-MacBook-Pro.local>
2022-07-08 21:41:31 +0100

    fixup! Setup folder aliases

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import V1Page from "@components/v1/Home"
-import V2Page from "@components/v2/Home"
-import { getCurrentBooksFromOku } from "lib/oku.js"
-import DragWindow from "@components/DragWindow"
-import VersionHistory from "@components/VersionHistory"
+import V1Page from "@/components/v1/Home"
+import V2Page from "@/components/v2/Home"
+import { getCurrentBooksFromOku } from "@/lib/oku.js"
+import DragWindow from "@/components/DragWindow"
+import VersionHistory from "@/components/VersionHistory"
 
 export default function Home({ books }) {
   return (
