683b108b668f94e260fde4eb18c8c07a67e0c6b5
ben-greenwood <ben.224@hotmail.co.uk>
2022-07-09 11:46:36 +0100

    Add KBar

---
+++ components/v2/CommandBar/CommandBarResults.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { KBarResults, useMatches } from "kbar"
+import CommandBarResultItem from "./CommandBarResultItem"
+
+function CommandBarResults() {
+  const { results, rootActionId } = useMatches()
+
+  return (
+    <div className="bg-white ">
+      <KBarResults
+        items={results}
+        onRender={({ item, active }) =>
+          typeof item === "string" ? (
+            <div>{item}</div>
+          ) : (
+            <CommandBarResultItem
+              action={item}
+              active={active}
+              currentRootActionId={rootActionId}
+            />
+          )
+        }
+      />
+    </div>
+  )
+}
+
+export default CommandBarResults
