dbb8dfea2a98a0c264606bb6a169a78dc5de1e2d
ben-greenwood <ben.224@hotmail.co.uk>
2022-07-09 11:31:39 +0100

    Version history v1 design components

--- components/v1/Home/Bio.js
+++ components/v1/Home/Bio.js
@@ -1,10 +1,10 @@
 import React from "react"
-import ExternalLink from "../ExternalLink"
+import ExternalLink from "components/v1/ExternalLink"
 import { FireIcon, BookOpenIcon } from "@heroicons/react/outline"
-import Book from "../Book"
+import Book from "components/v1/Book"
 
 const Bio = ({ books }) => (
-  <div className="prose prose-orange dark:prose-invert dark:prose-orange max-w-none space-y-8 sm:space-y-10">
+  <div className="prose prose-orange max-w-none space-y-8 sm:space-y-10">
     <p>
       <span className="mr-1 text-lg">👋</span> I&apos;m a Product Engineer based
       in Bath UK, currently working at{" "}
