748e304642b5255444695fc0fb1bb624ee119028
ben-greenwood <ben.224@hotmail.co.uk>
2022-07-09 11:36:33 +0100

    Create draggable window to reveal v1

---
+++ components/Modal.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { motion } from "framer-motion"
+
+const Modal = ({ children, show, onClick, cta }) => {
+  if (!show) return null
+  // Handle outside click
+  // Implement transitions
+
+  return (
+    <div className="absolute top-0 left-0 h-full w-full">
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        ></motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.5 }}
+          transition={{ duration: 0.3, delay: 0.3 }}
+          className="fixed inset-0 z-10 overflow-y-auto"
+        >
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-8">
+              <div className="">{children}</div>
+              <div className="mt-5 sm:mt-6">
+                <button
+                  onClick={onClick}
+                  type="button"
+                  className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                >
+                  {cta}
+                </button>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import React from "react"
 import V1Page from "components/v1/Home/Home"
 import { getBooksFromOku } from "lib/oku"
+import DragWindow from "components/DragWindow"
+import VersionHistory from "components/VersionHistory"
 
 const Home = ({ books }) => {
-  return <V1Page books={books} />
+  return (
+    <div>
+      <DragWindow>
+        <div className="relative h-screen bg-white" />
+      </DragWindow>
+
+      <VersionHistory>
+        <V1Page books={books} />
+      </VersionHistory>
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
