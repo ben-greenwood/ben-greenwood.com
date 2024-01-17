36e2e911f507e69418c0830ccedf05be3ba5817f
ben-greenwood <ben.224@hotmail.co.uk>
2024-01-17 10:41:31 +0000

    Update banner

--- app/_components/FreelanceBanner.tsx
+++ app/_components/FreelanceBanner.tsx
@@ -4,9 +4,7 @@ export default function FreelanceBanner() {
       <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-slate-900 px-6 py-2.5 dark:bg-white sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
         <p className="text-sm leading-6 text-white dark:text-slate-800">
           <a href="mailto:ben.t.greenwood@gmail.com">
-            <strong className="font-semibold">
-              Available for Freelance Work
-            </strong>
+            <strong className="font-semibold">Available for Work</strong>
             <svg
               viewBox="0 0 2 2"
               className="mx-2 inline h-0.5 w-0.5 fill-current"
@@ -14,7 +12,7 @@ export default function FreelanceBanner() {
             >
               <circle cx={1} cy={1} r={1} />
             </svg>
-            Drop me a line to discuss availability&nbsp;
+            Drop me a line to discuss opportunities&nbsp;
             <span aria-hidden="true" className="ml-1">
               &rarr;
             </span>
